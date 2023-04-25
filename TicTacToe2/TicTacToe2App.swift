ab29195b1599ce6c8c1f6e4ccde135b15b458e7b
Wenpan Wu <wwu2@atb.com>
2023-04-24 21:55:08 -0600

    First check-in of TicTacToe

---
+++ TicTacToe2/TicTacToe2App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe2App.swift
+//  TicTacToe2
+//
+//  Created by Wu, Wenpan on 2023-04-24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
