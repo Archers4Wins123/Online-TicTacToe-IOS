bb3f88772cb104cfe158eea0e67eb942952a073e
Wenpan Wu <wwu2@atb.com>
2023-04-29 00:21:20 -0600

    Alert for win / tie works properly. Removed TicTacToeModel. Need to remove hard-coded 9 when rendering the field.

--- TicTacToe2/ContentView.swift
+++ TicTacToe2/ContentView.swift
@@ -24,93 +24,6 @@ class Square : ObservableObject {
     }
 }
 
-class TicTacToeModel : ObservableObject {
-    @Published var squares = [Square]()
-    
-    init() {
-        for _ in 0...8 {
-            squares.append(Square(status: .empty))
-        }
-    }
-    
-    func resetGame() {
-        for i in 0...8 {
-            squares[i].squareStatus = .empty
-        }
-     }
-    
-    private func checkIndexes(_ indexes : [Int]) -> SquareStatus? {
-        var homeCounter : Int = 0
-        var visitorCounter : Int = 0
-        for index in indexes {
-            let square = squares[index]
-            if square.squareStatus == .home {
-                homeCounter += 1
-            } else if square.squareStatus == .visitor {
-                visitorCounter += 1
-            }
-        }
-        if homeCounter == 3 {
-            return .home
-        } else if visitorCounter == 3 {
-            return .visitor
-        }
-        return nil
-    }
-    
-    var gameOver : (SquareStatus, Bool) {
-        get {
-            if thereIsAWinner != .empty {
-                return (thereIsAWinner, true)
-            } else {
-                for i in 0...8 {
-                    if squares[i].squareStatus == .empty {
-                        return (.empty, false)
-                    }
-                }
-                return (.empty, true)
-            }
-        }
-    }
-    private var thereIsAWinner: SquareStatus {
-        get {
-            if let check = self.checkIndexes([0, 1, 2]) {
-                return check
-            } else if let check = self.checkIndexes([3, 4, 5]) {
-                return check
-            } else if let check = self.checkIndexes([6, 7, 8]) {
-                return check
-            } else if let check = self.checkIndexes([0, 3, 6]) {
-                return check
-            } else if let check = self.checkIndexes([1, 4, 7]) {
-                return check
-            } else if let check = self.checkIndexes([2, 5, 8]) {
-                return check
-            } else if let check = self.checkIndexes([0, 4, 8]) {
-                return check
-            } else if let check = self.checkIndexes([2, 4, 6]) {
-                return check
-            }
-            return .empty
-        }
-    }
-    private func moveAI() {
-        var index = Int.random(in: 0...8)
-        while makeMove(index: index, player: .visitor) == false && gameOver.1 == false {
-            index = Int.random(in: 0...8)
-        }
-    }
-    func makeMove(index: Int, player: SquareStatus) -> Bool {
-        if squares[index].squareStatus == .empty {
-            squares[index].squareStatus = player
-            if player == .home {
-                moveAI()
-            }
-            return true
-        }
-        return false
-    }
-}
 
 struct SquareView : View {
     @ObservedObject var dataSource : Square
@@ -128,12 +41,10 @@ struct SquareView : View {
     }
 }
 struct ContentView: View {
-    @StateObject var ticTacToeModel = TicTacToeModel()
     @State var gameOver : Bool = false
     @StateObject var socketDelegate = WebSocket2()
     
     func buttonAction(_ index : Int) {
-        //print("Clicked \(index)")
         let makeTurn = MakeTurn(x: index%3, y: Int(index/3))
         do {
             let jsonData = try JSONEncoder().encode(makeTurn)
@@ -142,8 +53,6 @@ struct ContentView: View {
         } catch let error {
             print("Error in sending MakeTurn as JSON\n\(error)")
         }
-        _ = self.ticTacToeModel.makeMove(index: index, player: .home)
-        self.gameOver = self.ticTacToeModel.gameOver.1
     }
     var body: some View {
         VStack {
@@ -157,7 +66,7 @@ struct ContentView: View {
                 .foregroundColor(Color.black.opacity(0.7))
                 .padding(.bottom)
                 .font(.title2)
-            ForEach(0 ..< Int(ticTacToeModel.squares.count / 3), content: {
+            ForEach(0 ..< Int(9 / 3), content: {
                 row in
                 HStack {
                     ForEach(0 ..< 3, content: {
@@ -177,7 +86,7 @@ struct ContentView: View {
                                "\(self.socketDelegate.gameState.winningPlayer ?? " ") Win!" : "Nobody Wins"),
             dismissButton:
                     Alert.Button.destructive(Text("Ok"), action: {
-                self.ticTacToeModel.resetGame()
+                //self.ticTacToeModel.resetGame()
             }))
         }).onAppear {
             var request = URLRequest(url: URL(string: "ws://192.168.0.19:8080/play")!)
